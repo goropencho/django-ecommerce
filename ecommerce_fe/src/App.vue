26090da2def0e4a94ceb60e5543de51e90c100d9
Gaurav Panchal <gkp.125@gmail.com>
2024-08-13 00:10:19 +0530

    Minor Changes in Vue

--- ecommerce_fe/src/App.vue
+++ ecommerce_fe/src/App.vue
@@ -10,6 +10,7 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -17,7 +18,11 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        v-bind:class="{ 'is-active': showMobileMenu }"
+      >
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -46,6 +51,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
